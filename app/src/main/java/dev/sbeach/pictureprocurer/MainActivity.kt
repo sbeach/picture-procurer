b2224f42ed2bd9ed539b24c42ff53fab4ee72a76
Sean Beach <dirtybeach0728@gmail.com>
2020-03-02 02:21:22 -0600

    If large and original are missing, provide error message

--- app/src/main/java/dev/sbeach/pictureprocurer/MainActivity.kt
+++ app/src/main/java/dev/sbeach/pictureprocurer/MainActivity.kt
@@ -3,9 +3,11 @@ package dev.sbeach.pictureprocurer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.snackbar.Snackbar
 import dev.sbeach.pictureprocurer.data.model.flickr.Photo
 import dev.sbeach.pictureprocurer.ui.photo.PhotoFragment
 import dev.sbeach.pictureprocurer.ui.search.SearchFragment
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity(), SearchFragment.OnListFragmentInteractionListener {
 
@@ -20,10 +22,14 @@ class MainActivity : AppCompatActivity(), SearchFragment.OnListFragmentInteracti
     }
 
     override fun onListFragmentInteraction(item: Photo) {
-        val url = if (item.original != null && item.original.isNotEmpty())
-            item.original
-        else
-            item.large
+        val url = when {
+            !item.original.isNullOrBlank() -> item.original
+            !item.large.isNullOrBlank() -> item.large
+            else -> {
+                Snackbar.make(container, "Larger size is not available for this photo", Snackbar.LENGTH_SHORT).show()
+                return
+            }
+        }
         supportFragmentManager.beginTransaction()
             .add(R.id.container, PhotoFragment.newInstance(url), PhotoFragment.TAG)
             .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
